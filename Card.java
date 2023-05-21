ba0a2e7328c3994bdccc3bd514cb3eb04775d6f3
Lai Cheng Yung <laichengyuny@gmail.com>
2023-05-21 17:03:20 +0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,32 @@
+package Game;
+
+public class Card {
+    private String suit;//花色
+    private String rank;//数字
+ 
+    public Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+ 
+    public String getSuit() {
+        return suit;
+    }
+ 
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+ 
+    public String getRank() {
+        return rank;
+    }
+ 
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+ 
+    @Override
+    public String toString() {
+        return suit+rank;
+    }
+}
